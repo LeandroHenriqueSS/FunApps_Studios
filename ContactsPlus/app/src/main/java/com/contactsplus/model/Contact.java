767f1bd8f0cc9e5d26686516ee2330abe9778fbc
LeandroHenriqueSS <leandrohenriquecomp@gmail.com>
2017-07-21 14:28:00 -0300

    ContactPlus

---
+++ ContactsPlus/app/src/main/java/com/contactsplus/model/Contact.java
@@ -0,0 +1,42 @@
+package com.contactsplus.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface Contact extends Serializable {
+
+    // Contact Status
+    public int ADDED     = 1;
+    public int REQUESTED = 2;
+
+    /* Get Methods */
+    public int getID();
+
+    public String getName();
+    public String getPhone();
+    public String getEmail();
+
+    public String getRequestPlace();
+
+    public int getStatus();
+
+    public List<SocialMedia> getSocialMedias();
+
+    public byte[] getProfilePicture();
+
+
+    /* Set Methods */
+    public void setID(int ID);
+
+    public void setName(String name);
+    public void setPhone(String phone);
+    public void setEmail(String email);
+
+    public void setRequestPlace(String requestPlace);
+
+    public void setStatus(int status);
+
+    public void setSocialMedias(List<SocialMedia> socialMedias);
+
+    public void setProfilePicture(byte[] profilePicture);
+}
---
+++ ContactsPlus/app/src/test/java/com/contactsplus/ExampleUnitTest.java
@@ -0,0 +1,36 @@
+package com.contactsplus;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void insertContact() throws Exception {
+//        db = new ContactDB(context);
+//
+//        Contact contact = new ContactImpl();
+//        contact.setName("Name");
+//        contact.setPhone("999 9999 9999");
+//        contact.setEmail("test@test.com");
+//        contact.setRequestPlace("Street Test");
+//        contact.setStatus(Contact.ADDED);
+//
+//        boolean result = db.insertContact(contact);
+//
+//        assertEquals(result, true);
+    }
+
+
+
+
+}
